489a875261f3e2042a356e03ab8bed385392b466
Lukas Wolfsteiner <lukas@wolfsteiner.de>
2016-02-09 16:28:52 +0100

    Implement permission handling using Dexter Ref: https://github.com/Karumi/Dexter

--- app/src/main/java/de/dotwee/sharecrypter/view/MainActivity.java
+++ app/src/main/java/de/dotwee/sharecrypter/view/MainActivity.java
@@ -1,5 +1,6 @@
 package de.dotwee.sharecrypter.view;
 
+import android.Manifest;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -7,6 +8,14 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -18,7 +27,7 @@ import timber.log.Timber;
 /**
  * Created by Lukas Wolfsteiner on 03.02.2016.
  */
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, PermissionListener {
     private static final String LOG_TAG = "MainActivity";
 
     @Bind(R.id.dialogTitle)
@@ -44,6 +53,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         // Applying activity layout
         setContentView(R.layout.activity_main);
+
+        // Check for permission
+        Dexter.checkPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        Dexter.checkPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
 
         ButterKnife.bind(this);
         Timber.tag(LOG_TAG);
@@ -93,5 +106,40 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
 
         }
+    }
+
+    /**
+     * Method called whenever a requested permission has been granted
+     *
+     * @param response A response object that contains the permission that has been requested and
+     *                 any additional flags relevant to this response
+     */
+    @Override
+    public void onPermissionGranted(PermissionGrantedResponse response) {
+
+    }
+
+    /**
+     * Method called whenever a requested permission has been denied
+     *
+     * @param response A response object that contains the permission that has been requested and
+     *                 any additional flags relevant to this response
+     */
+    @Override
+    public void onPermissionDenied(PermissionDeniedResponse response) {
+        Toast.makeText(this, getString(R.string.message_permission_necessary), Toast.LENGTH_LONG).show();
+        this.finish();
+    }
+
+    /**
+     * Method called whenever Android asks the application to inform the user of the need for the
+     * requested permission. The request process won't continue until the token is properly used
+     *
+     * @param permission The permission that has been requested
+     * @param token      Token used to continue or cancel the permission request process. The permission
+     */
+    @Override
+    public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+
     }
 }
