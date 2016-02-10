c87141e59be7795d20bc1f26f83fc24c300a0514
Lukas Wolfsteiner <lukas@wolfsteiner.de>
2016-02-10 15:13:25 +0100

    Remove unused dependencies and usages

--- app/src/test/java/de/dotwee/sharecrypter/ShareCrypterApplicationTest.java
+++ app/src/test/java/de/dotwee/sharecrypter/ShareCrypterApplicationTest.java
@@ -1,14 +1,8 @@
 package de.dotwee.sharecrypter;
 
-import org.junit.runner.RunWith;
-import org.robolectric.RobolectricGradleTestRunner;
-import org.robolectric.annotation.Config;
-
 /**
  * Created by Lukas Wolfsteiner on 06.02.2016.
  */
-@RunWith(RobolectricGradleTestRunner.class)
-@Config(constants = BuildConfig.class)
 public class ShareCrypterApplicationTest {
 
 }
--- app/src/test/java/de/dotwee/sharecrypter/model/utils/StringUtilsTest.java
+++ app/src/test/java/de/dotwee/sharecrypter/model/utils/StringUtilsTest.java
@@ -1,19 +1,12 @@
 package de.dotwee.sharecrypter.model.utils;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.robolectric.RobolectricGradleTestRunner;
-import org.robolectric.annotation.Config;
-
-import de.dotwee.sharecrypter.BuildConfig;
 
 import static org.junit.Assert.assertEquals;
 
 /**
  * Created by Lukas Wolfsteiner on 06.02.2016.
  */
-@RunWith(RobolectricGradleTestRunner.class)
-@Config(constants = BuildConfig.class)
 public class StringUtilsTest {
 
     @Test
